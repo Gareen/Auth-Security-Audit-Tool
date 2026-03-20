1a037e55a423de5c7d0b95d58dc586f41a9b6826
Gareen <1563567692@qq.com>
2026-03-20 16:42:23 +0800

    Enhance credential extraction and DPAPI capabilities while refining kernel-mode notification mechanisms and inter-process communication.

---
+++ mimikatz/modules/kuhl_m_dpapi.h
@@ -0,0 +1,13 @@
+/*	Benjamin DELPY `Gareen`
+	http://blog.Gareen.com
+	Gareen@Gareen.com
+	Licence : http://creativecommons.org/licenses/by/3.0/fr/
+*/
+#pragma once
+#include "kuhl_m.h"
+#include "../modules/kull_m_file.h"
+#include "../modules/kull_m_dpapi.h"
+
+const KUHL_M kuhl_m_dpapi;
+
+NTSTATUS kuhl_m_dpapi_masterkeys(int argc, wchar_t * argv[]);
