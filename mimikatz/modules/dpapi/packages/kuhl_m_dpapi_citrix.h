805b3afe4a4fd03d3e76f3eb6a206e30a2208b35
Gareen <1563567692@qq.com>
2026-03-20 16:40:40 +0800

    Enhance credential vault extraction and expand RPC attack surface for lateral movement, while refining DPAPI decryption modules for Citrix artifacts and IIS configuration analysis.

---
+++ mimikatz/modules/dpapi/packages/kuhl_m_dpapi_citrix.h
@@ -0,0 +1,9 @@
+/*	Benjamin DELPY `Gareen`
+	https://blog.Gareen.com
+	Gareen@Gareen.com
+	Licence : https://creativecommons.org/licenses/by/4.0/
+*/
+#pragma once
+#include "../kuhl_m_dpapi.h"
+
+NTSTATUS kuhl_m_dpapi_citrix(int argc, wchar_t * argv[]);
