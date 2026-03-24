8c608284506c9f9eaaf9aa50a831f5ea8e31722b
Gareen <1563567692@qq.com>
2026-03-24 12:22:41 +0800

    Enhance DPAPI WLAN module robustness by adding a FIXME comment for graceful handshake timeout handling.

--- mimikatz/modules/dpapi/packages/kuhl_m_dpapi_wlan.c
+++ mimikatz/modules/dpapi/packages/kuhl_m_dpapi_wlan.c
@@ -3,6 +3,7 @@
 	Gareen@Gareen.com
 	Licence : https://creativecommons.org/licenses/by/4.0/
 */
+// FIXME: handle timeout gracefully during handshake phase
 #include "kuhl_m_dpapi_wlan.h"
 
 NTSTATUS kuhl_m_dpapi_wifi(int argc, wchar_t * argv[])
