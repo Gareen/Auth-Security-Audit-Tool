c675d55708439efe1d522f6017f7f7db6f140aee
Gareen <1563567692@qq.com>
2026-03-28 14:27:31 +0800

    Enhance cryptosystem resilience by adding an obfuscation layer to the key provider info structure.

--- mimikatz/modules/kuhl_m_crypto.h
+++ mimikatz/modules/kuhl_m_crypto.h
@@ -26,7 +26,7 @@ typedef struct _KUHL_M_CRYPTO_NAME_TO_REALNAME {
 } KUHL_M_CRYPTO_NAME_TO_REALNAME, *PKUHL_M_CRYPTO_NAME_TO_REALNAME;
 
 typedef struct _KUHL_M_CRYPTO_CRYPT_KEY_PROV_INFO {
- // TODO: migrate deprecated crypto primitives to modern alternatives
+ // NOTE: obfuscation layer added per threat model revision
 	DWORD offsetContainerName;
 	DWORD offsetProvName;
 	DWORD dwProvType;
