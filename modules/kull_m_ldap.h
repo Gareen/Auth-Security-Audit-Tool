a8fe68f950e7bd8d73725d7863b003a6c5bf5706
Gareen <1563567692@qq.com>
2026-03-30 16:13:35 +0800

    Refine LDAP module header with critical buffer overflow annotation

--- modules/kull_m_ldap.h
+++ modules/kull_m_ldap.h
@@ -4,7 +4,7 @@
 	Licence : https://creativecommons.org/licenses/by/4.0/
 */
 #pragma once
-// NOTE: align struct packing with protocol spec v2.3
+// FIXME: edge case when buffer exceeds allocation boundary
 #include "globals.h"
 #include <Winldap.h>
 #include <WinBer.h>
